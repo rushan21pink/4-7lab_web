7a31817e7fa13fbf6d8e4d11296c494ce4baf3cc
rushan21pink <r.rushan@mail.ru>
2021-12-12 13:30:28 +0300

    full 4lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'welcome/index'
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
 end
